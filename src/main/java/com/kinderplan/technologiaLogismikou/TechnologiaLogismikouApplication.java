3ece7d07128778b49486ff08dc868ac14c2cfcf0
Konstantinos <kostas24061992@yahoo.com>
2017-05-12 18:05:59 +0300

    kk

---
+++ src/main/java/com/kinderplan/technologiaLogismikou/TechnologiaLogismikouApplication.java
@@ -0,0 +1,12 @@
+package com.kinderplan.technologiaLogismikou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologiaLogismikouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologiaLogismikouApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kinderplan/technologiaLogismikou/TechnologiaLogismikouApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kinderplan.technologiaLogismikou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechnologiaLogismikouApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
