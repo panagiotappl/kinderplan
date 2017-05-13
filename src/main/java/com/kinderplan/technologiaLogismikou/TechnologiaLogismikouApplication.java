f86c362e30c86808c360b9ac105771e44e44f5aa
Panos <paaparrigopoulosp@gmail.com>
2017-05-13 14:53:16 +0300

    * Inits web. * Adds first endpoint.

--- src/main/java/com/kinderplan/technologiaLogismikou/TechnologiaLogismikouApplication.java
+++ src/main/java/com/kinderplan/technologiaLogismikou/TechnologiaLogismikouApplication.java
@@ -1,7 +1,12 @@
 package com.kinderplan.technologiaLogismikou;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TechnologiaLogismikouApplication {
@@ -9,4 +14,6 @@ public class TechnologiaLogismikouApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TechnologiaLogismikouApplication.class, args);
 	}
+
+
 }
