901bdcc28714d7992358918aa3d2e89d61bd6671
KechagiasKonstantinos <kostas24061992@yahoo.com>
2017-05-20 16:15:45 +0300

    Config changes KK

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -5,10 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackageClasses = UserController.class)
 @SpringBootApplication
-@ComponentScan(basePackageClasses = UserController.class)
-
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/webapplication/dao/UserRepository.java
+++ src/main/java/com/webapplication/dao/UserRepository.java
@@ -1,14 +1,13 @@
 package com.webapplication.dao;
 import com.webapplication.entities.UsersEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
 @Repository
-public interface UserRepository extends CrudRepository<UsersEntity, Long> {
-
-    List<UsersEntity> findAll();
+public interface UserRepository extends JpaRepository<UsersEntity, Long> {
 
     UsersEntity findUserById(int id);
     UsersEntity findUserByEmailAndPassword(String email, String password);
