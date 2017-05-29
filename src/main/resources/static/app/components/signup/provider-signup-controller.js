984725da2aa1507c2856725fcc87ed38bdb97462
Panos Paparrigopoulos <paparrigopoulosp@gmail.com>
2017-05-29 18:50:44 +0300

    Adds provider signup functionality

--- src/main/resources/static/app/components/signup/provider-signup-controller.js
+++ src/main/resources/static/app/components/signup/provider-signup-controller.js
@@ -1,10 +1,9 @@
 
 router.controller('providerSignupController', function($scope, $cookies, UserService){
 
-    console.log('helo');
     $scope.credentials = {
         company_name: '',
-        mail: '',
+        email: '',
         password: '',
         rep_password: '',
         first_name: '',
@@ -17,12 +16,6 @@ router.controller('providerSignupController', function($scope, $cookies, UserSer
         afm: ''
     };
 
-    $scope.vaildateEmail = function(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    };
-
-
     $scope.signup = function() {
 
         $scope.options = {
@@ -33,18 +26,20 @@ router.controller('providerSignupController', function($scope, $cookies, UserSer
 
 
         var request = {
-            company_name: $scope.credentials.company_name,
-            mail: $scope.credentials.mail,
+            email: $scope.credentials.email,
             password: $scope.credentials.password,
-            rep_password: $scope.credentials.rep_password,
-            first_name: $scope.credentials.first_name,
-            last_name: $scope.credentials.last_name,
-            phone_number: $scope.credentials.phone_number,
+            repassword: $scope.credentials.rep_password,
+            role: 'provider',
+            name: $scope.credentials.first_name,
+            surname: $scope.credentials.last_name,
+            phone: $scope.credentials.phone_number,
             latitude: $scope.credentials.latitude,
             longitude: $scope.credentials.longitude,
             address: $scope.geoloc,
-            role: 'provider',
-            afm: $scope.credentials.afm
+            provider: {
+                companyName: $scope.credentials.company_name,
+                vatNumber: $scope.credentials.afm
+            }
         };
 
         var address = $scope.details.geometry.location;
@@ -52,7 +47,16 @@ router.controller('providerSignupController', function($scope, $cookies, UserSer
         request.latitude = address.lat();
         request.longitude = address.lng();
 
-        console.log(request)
+        console.log(request);
+
+        UserService.createUser(request)
+            .then(function(response){
+                console.log(response)
+
+            }, function(error){
+                console.log(error);
+            });
+
 
     }
 });
