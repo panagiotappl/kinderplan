85729c9ab7e5ec7d15d33ce23ac082e1f1ff8790
Panos Paparrigopoulos <paparrigopoulosp@gmail.com>
2017-05-31 15:05:32 +0300

    * Adds missing fields validation on sign up forms. * Fixes small bug on add event form.

--- src/main/resources/static/app/components/signup/parent-signup-controller.js
+++ src/main/resources/static/app/components/signup/parent-signup-controller.js
@@ -15,11 +15,6 @@ router.controller('parentSignupController', function($scope, $cookies, UserServi
         role: ''
     };
 
-    $scope.vaildateEmail = function(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    };
-
 
     $scope.signup = function() {
 
