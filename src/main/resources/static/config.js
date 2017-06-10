0ba69bc0853815e4b9495c8d8b67010e33f26260
Panos Paparrigopoulos <paparrigopoulosp@gmail.com>
2017-06-10 15:51:09 +0300

    * Adds events list mockup. * Closes #13.

--- src/main/resources/static/config.js
+++ src/main/resources/static/config.js
@@ -16,30 +16,36 @@ router.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider, $
             url: '/',
             templateUrl: 'app/components/homepage/homepage.html',
             controller: 'homepageController',
-            data: {pageTitle : ""}
+            data: {pageTitle : "Kinderplan"}
         })
         .state('providerSignup', {
             url: '/signup/provider',
             templateUrl: 'app/components/signup/provider_signup.html',
             controller: 'providerSignupController',
-            data: {pageTitle : "Signup - Provider"}
+            data: {pageTitle : "Kinderplan - Signup - Provider"}
         })
         .state('parentSignup', {
             url: '/signup/parent',
             templateUrl: 'app/components/signup/parent_signup.html',
             controller: 'parentSignupController',
-            data: {pageTitle : "Signup - Parent"}
+            data: {pageTitle : "Kinderplan - Signup - Parent"}
         })
         .state('signup', {
             url: '/signup',
             templateUrl: 'app/components/signup/signup.html',
             controller: 'signupController',
-            data: {pageTitle : "Sign Up"}
+            data: {pageTitle : "Kinderplan - Sign Up"}
         })
         .state('addEvent', {
             url: '/events/add',
             templateUrl: 'app/components/events/add_event.html',
             controller: 'addEventController',
-            data: {pageTitle : "Add event"}
+            data: {pageTitle : "Kinderplan - Add event"}
+        })
+        .state('listEvents', {
+            url: '/events/list',
+            templateUrl: 'app/components/events/list_events.html',
+            controller: 'listEventsController',
+            data: {pageTitle : "Kinderplan - Events"}
         });
 });
