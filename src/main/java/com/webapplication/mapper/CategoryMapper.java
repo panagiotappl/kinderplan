22c51c263acb6f0aec585061f53e1dbef9c91371
Panos Paparrigopoulos <paparrigopoulosp@gmail.com>
2017-06-21 16:03:03 +0300

    Fixes bugs on event submition.
    * Addresses #14 (closes last 2 checkboxes).

--- src/main/java/com/webapplication/mapper/CategoryMapper.java
+++ src/main/java/com/webapplication/mapper/CategoryMapper.java
@@ -14,11 +14,11 @@ import java.util.HashSet;
 public class CategoryMapper {
 	public HashSet<CategoryEntity> categoryEntityFromCategoryDto(HashSet<CategoryDto> categoriesDto){
 		HashSet<CategoryEntity> categoryEntities = new HashSet<CategoryEntity>(categoriesDto.size());
-		for (CategoryDto categoryDto : categoriesDto){
-			CategoryEntity categoryEntity = new CategoryEntity();
-			categoryEntity.setCategory(categoryDto.getCategory());
-			categoryEntities.add(categoryEntity);
-		}
+//		for (CategoryDto categoryDto : categoriesDto){
+//			CategoryEntity categoryEntity = new CategoryEntity();
+//			categoryEntity.setCategory(categoryDto.getCategory());
+//			categoryEntities.add(categoryEntity);
+//		}
 
 		return categoryEntities;
 	}
