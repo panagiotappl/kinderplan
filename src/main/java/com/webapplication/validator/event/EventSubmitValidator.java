da8d16f87be7776355f022e1f9e3a2e60e41c452
MaryLouPap <sdi1200095@di.uoa.gr>
2017-06-23 19:08:20 +0300

    provider (with authentication) in event submit

--- src/main/java/com/webapplication/validator/event/EventSubmitValidator.java
+++ src/main/java/com/webapplication/validator/event/EventSubmitValidator.java
@@ -1,6 +1,7 @@
 package com.webapplication.validator.event;
 
 import com.webapplication.dao.EventRepository;
+import com.webapplication.dao.ProviderRepository;
 import com.webapplication.dto.event.EventSubmitRequestDto;
 import com.webapplication.error.event.EventSubmitError;
 import com.webapplication.exception.ValidationException;
@@ -18,10 +19,13 @@ import java.util.Optional;
 public class EventSubmitValidator implements Validator <EventSubmitRequestDto> {
 	@Autowired
 	private EventRepository eventRepository;
+	@Autowired
+	private ProviderRepository providerRepository;
 	@Override
 	public void validate(EventSubmitRequestDto request) throws ValidationException {
 		Optional.ofNullable(request).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_DATA));
 		Optional.ofNullable(request.getName()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_NAME));
+		Optional.ofNullable(request.getProvider()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_PROVIDER));
 		Optional.ofNullable(request.getAddress()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_ADDRESS));
 		Optional.ofNullable(request.getAge_from()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_AGE_FROM));
 		Optional.ofNullable(request.getAge_to()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_AGE_TO));
@@ -31,6 +35,9 @@ public class EventSubmitValidator implements Validator <EventSubmitRequestDto> {
 		Optional.ofNullable(request.getDate_ending()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_DATE_ENDING));
 		Optional.ofNullable(request.getCategories()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_CATEGORY));
 		Optional.ofNullable(request.getDates()).orElseThrow(() -> new ValidationException(EventSubmitError.MISSING_DATES));
+		if (providerRepository.findProviderByUserId(request.getProvider()) == null) {
+			throw new ValidationException(EventSubmitError.NOT_A_PROVIDER);
+		}
 		//if (eventRepository.findEventsByNameAndProviderAndDate_starting(request.getName(), request.getProvider(), request.getDate_starting())!=null){
 		//	throw new ValidationException(EventSubmitError.EXISTING_EVENT);
 		//}
