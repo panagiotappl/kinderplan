c173307ae98ea963c09c35cf45c99df681c548fb
seintaridis <jimseinta@hotmail.com>
2017-06-30 17:07:45 +0200

    add forgotten file

--- src/main/java/com/webapplication/dao/jpaRepository/TransactionRepository.java
+++ src/main/java/com/webapplication/dao/jpaRepository/TransactionRepository.java
@@ -1,5 +1,9 @@
 package com.webapplication.dao.jpaRepository;
 
+<<<<<<< HEAD
+=======
+import com.webapplication.entity.ParentEntity;
+>>>>>>> add forgotten file
 import com.webapplication.entity.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +12,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
 
+<<<<<<< HEAD
 
+=======
+    TransactionEntity findTransactionById(Integer id);
+>>>>>>> add forgotten file
 }
