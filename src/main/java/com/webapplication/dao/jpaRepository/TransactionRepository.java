1f003f4f9b56fa818caf25353d7a915af1e9bb98
panagiotappl <panagiotappl@gmail.com>
2017-07-01 12:31:16 +0300

    search api

--- src/main/java/com/webapplication/dao/jpaRepository/TransactionRepository.java
+++ src/main/java/com/webapplication/dao/jpaRepository/TransactionRepository.java
@@ -1,13 +1,7 @@
 package com.webapplication.dao.jpaRepository;
-<<<<<<< HEAD
-import com.webapplication.entity.ParentEntity;
-=======
 
-<<<<<<< HEAD
-=======
 import com.webapplication.entity.ParentEntity;
->>>>>>> add forgotten file
->>>>>>> add forgotten file
+import com.webapplication.entity.ParentEntity;
 import com.webapplication.entity.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,13 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
 
-<<<<<<< HEAD
     TransactionEntity findTransactionById(Integer id);
-=======
-<<<<<<< HEAD
->>>>>>> add forgotten file
-
-=======
-    TransactionEntity findTransactionById(Integer id);
->>>>>>> add forgotten file
 }
